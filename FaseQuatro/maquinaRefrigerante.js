78a903a2edadd2755c39a965ef54d72b60b1e7e0
leandrofita <89036986+leandrofita@users.noreply.github.com>
2021-08-20 23:08:41 -0300

    update do código
    Totalmente funcional, porém cabendo alguns updates, como opção de inserir mais de uma cédula.

--- FaseQuatro/maquinaRefrigerante.js
+++ FaseQuatro/maquinaRefrigerante.js
@@ -1,4 +1,5 @@
 console.log('MÁQUINA DE REFRIGERANTES \n Por favor, escolha o sabor do seu refrigerante: ')
+
 var prompt = require('prompt-sync')();
 var coca1 = 4
 var fanta2 = 3
@@ -12,10 +13,60 @@ var cedula50 = 50
 
 var preço = 0
 var voltar = 'V' || 'v';
- 
- for (voltar =0 ; voltar < 5 ; voltar++) {
-    var sabor = prompt("Digite o número relativo ao sabor correspondente: \n 1 - Coca \n 2 - Fanta \n 3- Guaraná \n 4- Pepsi\n -> ");
-      if (sabor == "1") {
+var comprou = false;
+
+
+function comprar()
+{
+  console.log('Opções de pagamento: ',);     
+
+  var negativo = true // teste que fornece a condição do while
+
+  while (negativo)  // loop enquanto essa condição se manter verdadeira...
+  {
+    var pagamento =prompt('Pagamento:\n Escolha o valor da cédula:R$ \n- 2 \n- 5 \n- 10 \n- 50\n ->');
+
+          if (pagamento == "2")
+            pagamento = cedula2;
+          else
+          if (pagamento == "5")
+            pagamento = cedula5;
+          else
+          if (pagamento == "10")
+              pagamento = cedula10;
+          else 
+          if (pagamento == "50")
+                pagamento = cedula50;;
+          if (pagamento == voltar) 
+          {
+          
+            };
+    var troco = (pagamento - preço);
+
+  var mensagem = ""
+
+    negativo = (Math.sign(troco) == -1);  // função Match.sign me diz se um número é positivo ou negativo, se for positivo me devolve "1" negativo "-1"
+
+    if (negativo) {
+      mensagem = "Valor insuficiente, por favor insira um valor válido."  // O teste do if não funcionou aqui sem um else
+      console.log(mensagem)
+    
+            }
+    else {
+      mensagem = "Seu troco é de: R$ " // Não consegui exibir a mensagem incluindo a variável troco nessa linha.
+      console.log(mensagem, troco);
+      console.log('Obrigado pela sua compra!');
+      comprou = true; 
+    };              
+  }
+
+}
+
+
+for (let cont = 0 ; cont < 4 ; cont++) {
+  var sabor = prompt("Digite o número relativo ao sabor correspondente: \n 1 - Coca \n 2 - Fanta \n 3- Guaraná \n 4- Pepsi\n -> ");
+  //var sabor = "1";    
+  if (sabor == "1") {
         console.log('Excelente escolha! Valor:R$',coca1);
         preço = coca1
       }
@@ -34,47 +85,21 @@ var voltar = 'V' || 'v';
             console.log('Excelente escolha! Valor:R$',pepsi4);
             preço = pepsi4
             } 
-          voltar = prompt('Digite "V" caso queira retornar para a seleção ou "C" para confirmar e prosseguir com a compra: \n->');
+
+  voltar = prompt('Digite "V" caso queira retornar para a seleção ou "C" para confirmar e prosseguir com a compra: \n->');
+  if ((voltar == 'C') || (voltar == 'c')) {
+    comprar()  
+      if (comprou = true){
+          cont = 4;
+      }   
+       
+    }  
+      if (!comprou){
+        console.log('Nenhuma opção válida selecionada - Por favor, reinicie a operação :)');} 
 }
-  console.log('Opções de pagamento: ',);     
+
   
 
-var negativo = true // teste que fornece a condição do while
-
-while (negativo)  // loop enquanto essa condição se manter verdadeira...
-{
-  var pagamento =prompt('Pagamento:\n Escolha o valor da cédula:R$ \n- 2 \n- 5 \n- 10 \n- 50\n ->');
-
-        if (pagamento == "2")
-          pagamento = cedula2;
-        else
-           if (pagamento == "5")
-           pagamento = cedula5;
-          else
-             if (pagamento == "10")
-             pagamento = cedula10;
-            else 
-              if (pagamento == "50")
-              pagamento = cedula50;;
-                if (pagamento == voltar) {
-                for (voltar = 0; voltar < 3; voltar++);
-          };
-  var troco = (pagamento - preço);
-
-var mensagem = ""
-
-   negativo = (Math.sign(troco) == -1);  // função Match.sign me diz se um número é positivo ou negativo, se for positivo me devolve "1" negativo "-1"
-
-  if (negativo) {
-    mensagem = "Valor insuficiente, por favor insira um valor válido."  // O teste do if não funcionou aqui sem um else
-    console.log(mensagem)
-   
-          }
-  else {
-    mensagem = "Seu troco é de: R$ " // Não consegui exibir a mensagem incluindo a variável troco nessa linha.
-    console.log(mensagem, troco);
-  };              
-}
   /* Match.sign()
   
   Math.sign(3);     //  1
